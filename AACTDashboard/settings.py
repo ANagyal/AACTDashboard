9252fbc31af9b70cc7943e746b863859490dbe19
AjayPaul Nagyal <asn68@cornell.edu>
2025-05-19 00:35:48 -0400

    Added styling to login and dashboard pages
    - Updated static files directory in settings.py

--- AACTDashboard/settings.py
+++ AACTDashboard/settings.py
@@ -128,8 +128,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+
+# For development only (not needed for production with collectstatic)
+STATICFILES_DIRS = [
+    BASE_DIR / "dashboard/static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
