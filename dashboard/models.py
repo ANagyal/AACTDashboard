2eb38bcf9bb1337cf796e7a1272f8de4927e7b98
AjayPaul Nagyal <asn68@cornell.edu>
2025-05-19 09:44:24 -0400

    update the DashboardUser model to include total trial counts

--- dashboard/models.py
+++ dashboard/models.py
@@ -5,6 +5,9 @@ class DashboardUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=255, blank=True)
     trials = models.JSONField(default=list)
+    total_trials = models.IntegerField(default=0)
+    trials_inprogress = models.IntegerField(default=0)
+    trials_missingcontact = models.IntegerField(default=0)
 
     def __str__(self):
         return self.user.username
